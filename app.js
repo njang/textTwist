20c4ee4b0d41473dbd48e60fb1b8ebf9afbc0fd7
njang <neo.w.jang@gmail.com>
2017-12-21 19:47:53 -0600

    Add favicon

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 // Load the list of words from a separate words.json file.
+// Filter out words that are too short or too long.
 let wordList = [];
-function load() {
-	$.getJSON("words.json", (result) => {
-    wordList = result.words;
+$(document).ready(function() {
+	let minLength = 3;	// minimum string length of word
+	let maxLength = 8;	// maximum string length of word
+  $.getJSON("words.json", (result) => {
+    wordList = result.words.filter(word => (word.length <= maxLength && word.length >= minLength));
   });
+});
+
+// Pick a random word from the list.
+let randomWord;
+const pickRandomWord = () => {
+	let index = Math.round(Math.random() * (wordList.length - 1));
+	randomWord = wordList[index];
+	console.log(randomWord);
+}
+
+// Build a list of permutations
+let permutations = [];
+const buildPermutations = () => {
+	let letters = randomWord.split('').sort();
+	// letters.sort((a, b) => {return a - b});
+	console.log(letters);
 }
