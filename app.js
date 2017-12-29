719a6a83fc2ede9a3821be5e0208961554049c95
njang <neo.w.jang@gmail.com>
2017-12-28 23:23:35 -0600

    Fix bug caused by selecting from 2 separate word sources

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 // Load the list of words from a separate words.json file.
 // Filter out words hat are too short or too long.
 let wordList = [];
-let commonList = [];
+// let commonList = [];
 $(document).ready(function() {
 	let minLength = 3;	// minimum string length of word
 	let maxLength = 6;	// maximum string length of word
 	wordList = dictionary.words.filter(word => (word.length <= maxLength && word.length >= minLength));
-	commonList = common.words.filter(word => (word.length <= maxLength && word.length >= minLength));
+	// commonList = common.words.filter(word => (word.length <= maxLength && word.length >= minLength));
   // $.getJSON("words.json", (result) => {
   //   wordList = result.words.filter(word => (word.length <= maxLength && word.length >= minLength));
   // });
@@ -21,8 +21,8 @@ $(document).ready(function() {
 // Pick a random word from the list.
 let randomWord;
 const pickRandomWord = () => {
-	let index = Math.round(Math.random() * (commonList.length - 1));
-	randomWord = commonList[index];
+	let index = Math.round(Math.random() * (wordList.length - 1));
+	randomWord = wordList[index];
 }
 
 // Build a list of permutations using the letters of the random word.
